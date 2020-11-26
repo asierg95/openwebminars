26f8fefb204a0cb7fbc0321c0bacd222f3f2f227
asierg95 <asierga95@gmail.com>
2020-11-26 09:20:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ touch /home/ikerlan/scriptOK
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
-        --containerRegistryURL)                HOST_NAME="$2" ;;
+        --runtime)                      APP_RUNTIME="$2" ;;
+        --containerRegistryURL)                HOST_NAME="$2" ;; 
     esac
     shift
 done
 
+touch /home/ikerlan/${APP_RUNTIME}
 touch /home/ikerlan/${HOST_NAME}
